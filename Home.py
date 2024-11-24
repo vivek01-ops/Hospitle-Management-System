9ef981582a25b338a6fc3894724ed8820d0e2ee9
vivek01-ops <https://github.com/vivek01-ops>
2024-11-24 11:43:22 +0530

    renamed

--- Home.py
+++ Home.py
@@ -25,17 +25,17 @@ col1, col2 = st.columns(2)
 
 with col1:
     if st.button("🛌 Add new Patient", use_container_width=True):
-        st.switch_page("pages/patient.py") 
+        st.switch_page("pages/1_Patient.py") 
     
     if st.button("🧾 Create Bill", use_container_width=True):
-        st.switch_page("pages/billing.py") 
+        st.switch_page("pages/4_Billing.py") 
     
 with col2:
     if st.button("👨🏻‍⚕️ Add new Doctor", use_container_width=True):
-        st.switch_page("pages/doctor.py")
+        st.switch_page("pages/2_Doctor.py")
 
     if st.button("🗓️ Crate new Appointment", use_container_width=True):
-        st.switch_page("pages/appointment.py")
+        st.switch_page("pages/3_Appointment.py")
     
     
     
